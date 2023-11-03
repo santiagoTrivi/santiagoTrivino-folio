ca1003144d83f645cda752ba544e8f4bd7e709c2
santiagoTrivi <santiagocarvajal103@gmail.com>
2023-11-02 21:25:12 -0400

    dev: submit button effect removed

--- src/components/buttonSubmit.tsx
+++ src/components/buttonSubmit.tsx
@@ -3,8 +3,8 @@ import { FaPaperPlane } from "react-icons/fa"
 
 export const ButtonSubmit = () => {
     return(
-        <button className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65" type="submit">
-            Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"/>
+        <button className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all  hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65" type="submit">
+            Submit <FaPaperPlane className="text-xs opacity-70 transition-all "/>
         </button>
     )
 }
