1b09a809b40f8cd9559c258ba9383101a4d0085d
santiagoTrivi <santiagocarvajal103@gmail.com>
2023-11-02 21:42:24 -0400

    dev: submit buttom img changed

--- src/components/buttonSubmit.tsx
+++ src/components/buttonSubmit.tsx
@@ -1,10 +1,10 @@
-import { FaPaperPlane } from "react-icons/fa"
+import paperplane from '/paper-plane.png';
 
 
 export const ButtonSubmit = () => {
     return(
-        <button className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all  hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65" type="submit">
-            Submit <FaPaperPlane className="text-xs opacity-70 transition-all "/>
+        <button className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65" type="submit">
+            Submit <img src={paperplane} className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 w-4 h-4" alt="" />
         </button>
     )
 }
